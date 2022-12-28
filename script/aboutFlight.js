0b40658e4c1c24df14257fc62435a6b1345e5932
narenjak <narenjak376@gmail.com>
2022-12-28 10:30:17 +0330

    some change in style

--- script/aboutFlight.js
+++ script/aboutFlight.js
@@ -2,6 +2,7 @@ function myAboutFlight() {
     var x;
     x = document.getElementById("res");
     for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";         
+        x[i].style.display.backgroundColor = "yellow";         
     }
 }
+myAboutFlight();
